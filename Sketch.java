78badc43aeda3d0c0e2c2db3639f773ab5b1b7b9
JohnDuarte23 <99973293+JohnDuarte23@users.noreply.github.com>
2022-04-02 00:11:50 -0400

    Update Sketch.java

--- Sketch.java
+++ Sketch.java
@@ -4,10 +4,11 @@ public class Sketch extends PApplet {
 	
 	
   /**
-   * Called once at the beginning of execution, put your size all in this method
+   * This code produces a patterned image
+   * @Author: JohnDuarte23
    */
   public void settings() {
-	// put your size call here
+	  // put your size call here
     size(1200, 600);
   }
 
@@ -21,17 +22,9 @@ public class Sketch extends PApplet {
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
-   */
+   */  
   public void draw() {
-	  
-	// sample code, delete this stuff
-    /*
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
-*/
     draw_section_outlines();
     draw_section1();
     draw_section2();
@@ -42,7 +35,6 @@ public class Sketch extends PApplet {
     draw_section6();
     draw_section7();
     draw_section8();
-
     
   }
 
@@ -68,16 +60,17 @@ public class Sketch extends PApplet {
   }
   
   /**
-   * draws the bottom left section
+   * Draws the bottom left section
    */
+  
   public void draw_section1(){
     int intX = 0;
     int intY = 0;
 
-    for(int intRow = 0; intRow < 30; intRow++){
-      for(int intColumn = 0; intColumn < 30; intColumn++){
-        intX = 3 + 0;  //Instead of zero, calculate the proper intX location using 'intRow'
-        intY = 300 + 3 + 0; //Instead of zero, calculate the proper intY location using 'intColumn'
+    for(int intRow = 0; intRow < 30; intRow ++){
+      for(int intColumn = 0; intColumn < 30; intColumn ++){
+        intX = intRow * 10;  //Instead of zero, calculate the proper intX location using 'intRow'
+        intY = 300 + intColumn * 10; //Instead of zero, calculate the proper intY location using 'intColumn'
 
         fill(255);
         noStroke();
@@ -86,52 +79,175 @@ public class Sketch extends PApplet {
       }
     }
   }
-
+  
   /**
    * Use the modulus operator and an if statement to select the color
    * Don't loop from 30 to 60 to shift everything over, just add 300 to x.
    */
-  public void draw_section2(){
 
+   // Draws section 2
+  public void draw_section2(){
+    int intX = 0;
+    int intY = 0;
+
+    for(int intRow2 = 0; intRow2 < 30; intRow2 ++){
+      for(int intColumn2 = 0; intColumn2 < 30; intColumn2 ++){
+        intX = 300 + intRow2 * 10;  
+        intY = 300 + intColumn2 * 10; 
+
+          if(intRow2 % 2 == 0){
+            fill(255);
+            noStroke();
+            rect(intX, intY, 5, 5);
+          }
+          else{
+            fill(0);
+            noStroke();
+            rect(intX, intY, 5, 5);
+          }  
+      }
+    }
   }
 
   /**
    * Use the modulus operator and an if/else statement to select the color.
    * Don't use multiple 'if' statements.
    */
-  public void draw_section3(){
 
+   // Draws section 3
+  public void draw_section3(){
+    int intX = 0;
+    int intY = 0;
+
+    for(int intRow3 = 0; intRow3 < 30; intRow3 ++){
+      for(int intColumn3 = 0; intColumn3 < 30; intColumn3 ++){
+        intX = 600 + intRow3 * 10;  
+        intY = 300 + intColumn3 * 10; 
+
+          if(intColumn3 % 2 == 0){
+            fill(255);
+            noStroke();
+            rect(intX, intY, 5, 5);
+          }
+          else{
+            fill(0);
+            noStroke();
+            rect(intX, intY, 5, 5);
+          }  
+      }
+    }
   }
 
   /**
    * Use the modulus operator and just one 'if' statement to select the color.
    */
+  
+   // Draws section 4
   public void draw_section4(){
+    int intX = 0;
+    int intY = 0;
 
+    for(int intRow4 = 0; intRow4 < 30; intRow4 ++){
+      for(int intColumn4 = 0; intColumn4 < 30; intColumn4 ++){
+        intX = 900 + intRow4 * 10;  
+        intY = 300 + intColumn4 * 10; 
+
+          if((intColumn4 % 2 == 0) && (intRow4 % 2 ==0)){
+            fill(255);
+            noStroke();
+            rect(intX, intY, 5, 5);
+          }
+          else{
+            fill(0);
+            noStroke();
+            rect(intX, intY, 5, 5);
+          }  
+      }
+    }
   }
 
   /**
    * Do NOT use 'if' statements to complete 5-8. Manipulate the loops instead
    */
+
+   // Draws section 5
   public void draw_section5(){
+    int intX = 0;
+    int intY = 0;
+    int intCounter = 30;
 
+    for(int intColumn5 = 0; intColumn5 < 30; intColumn5 ++){
+      for(int intRow5 = 30; intRow5 > intCounter; intRow5 --){
+          intX = intRow5 * 10;  
+          intY = intColumn5 * 10; 
+
+          fill(255);
+          noStroke();
+          rect(intX, intY, 5, 5);
+          
+      }
+      intCounter --;
+    }
   }
 
+  // Draws section 6
   public void draw_section6(){
+    int intX = 0;
+    int intY = 0;
+    int intCounter = 30;
 
+    for(int intColumn6 = 30; intColumn6 > 0; intColumn6 --){
+      for(int intRow6 = 0; intRow6 < intCounter; intRow6 ++){
+          intX = 300 + intRow6 * 10;  
+          intY = intColumn6 * 10; 
+
+          fill(255);
+          noStroke();
+          rect(intX, intY, 5, 5);
+          
+      }
+      intCounter --;
+    }
   }
 
+  // Draws section 7
   public void draw_section7(){
+    int intX = 0;
+    int intY = 0;
+    int intCounter = 30;
 
+    for(int intRow7 = 0; intRow7 < 30; intRow7 ++){
+      for(int intColumn7 = 0; intColumn7 < intCounter; intColumn7 ++){
+          intX = 600 + intRow7 * 10;  
+          intY = intColumn7 * 10; 
+
+          fill(255);
+          noStroke();
+          rect(intX, intY, 5, 5);
+      }
+      intCounter --;
+    }
   }
   
+  // Draws section 8
   public void draw_section8(){
+    int intX = 0;
+    int intY = 0;
+    int intCounter = 0;
 
+    for(int intRow8 = 0; intRow8 < 30; intRow8 ++){
+      for(int intColumn8 = 0; intColumn8 < intCounter; intColumn8 ++){
+          intX = 900 + intRow8 * 10;  
+          intY = intColumn8 * 10; 
+
+          fill(255);
+          noStroke();
+          rect(intX, intY, 5, 5);
+        
+      }
+
+      intCounter ++;
+      
+    }
   }
-
-
-
-
-
-
 }
